d799086d6c7d0071784cce490ae9e541346da05a
planetchili <planetchili@users.noreply.github.com>
2017-11-16 13:11:14 +0900

    simple example

--- Inheritance/Main.cpp
+++ Inheritance/Main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include <string>
 
+class A
+{
+public:
+	void FuncX()
+	{}
+private:
+	int var1;
+};
+
+// use ": public Parent" to inherit from another class
+class B : public A
+{
+public:
+	void FuncY()
+	{}
+private:
+	float var2;
+};
+
 int main()
 {
+	A mya;
+	B myb;
+
+	std::cout << sizeof( mya ) << std::endl;
+	std::cout << sizeof( myb ) << std::endl;
+
+	std::cin.get();
 	return 0;
 }
