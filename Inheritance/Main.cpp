819cfa88da75e0f1ebe0f2d43a1bf4b4f42df605
planetchili <planetchili@users.noreply.github.com>
2017-11-18 19:38:38 +0900

    memefighter basic inheritance implemented

--- Inheritance/Main.cpp
+++ Inheritance/Main.cpp
@@ -20,6 +20,131 @@ private:
 	std::mt19937 rng = std::mt19937( std::random_device{}() );
 };
 
+class MemeFighter
+{
+public:
+	const std::string& GetName() const
+	{
+		return name;
+	}
+	bool IsAlive() const
+	{
+		return hp > 0;
+	}
+	int GetInitiative() const
+	{
+		return speed + Roll( 2 );
+	}
+	void Punch( MemeFighter& other ) const
+	{
+		if( IsAlive() && other.IsAlive() )
+		{
+			std::cout << name << " punches " << other.GetName()
+				<< "!" << std::endl;
+			ApplyDamageTo( other,power + Roll( 2 ) );
+		}
+	}
+	void Tick()
+	{
+		if( IsAlive() )
+		{
+			const int recovery = Roll();
+			std::cout << name << " recovers " << recovery << " HP." << std::endl;
+			hp += recovery;
+		}
+	}
+protected:
+	MemeFighter( const std::string& name,int hp,int speed,int power )
+		:
+		name( name ),
+		hp( hp ),
+		speed( speed ),
+		power( power )
+	{
+		std::cout << name << " enters the ring!" << std::endl;
+	}
+	void ApplyDamageTo( MemeFighter& target,int damage ) const
+	{
+		target.hp -= damage;
+		std::cout << target.name << " takes " << damage << " damage." << std::endl;
+		if( !target.IsAlive() )
+		{
+			std::cout << "As the life leaves " << target.name << "'s body, so does the poop." << std::endl;
+		}
+	}
+	int Roll( int nDice = 1 ) const
+	{
+		return d.Roll( nDice );
+	}
+protected:
+	int hp;
+	int speed;
+	int power;
+	std::string name;
+private:
+	mutable Dice d;
+};
+
+class MemeFrog : public MemeFighter
+{
+public:
+	MemeFrog( const std::string& name )
+		:
+		MemeFighter( name,69,7,14 )
+	{}
+	void SpecialMove( MemeFighter& other ) const
+	{
+		if( IsAlive() && other.IsAlive() )
+		{
+			if( Roll() > 4 )
+			{
+				std::cout << GetName() << " attacks " << other.GetName() << " with a rainbow beam!" << std::endl;
+				ApplyDamageTo( other,Roll( 3 ) + 20 );
+			}
+			else
+			{
+				std::cout << GetName() << " falls off his unicycle." << std::endl;
+			}
+		}
+	}
+	void Tick()
+	{
+		if( IsAlive() )
+		{
+			std::cout << GetName() << " is hurt by the bad AIDS!" << std::endl;
+			ApplyDamageTo( *this,Roll() );
+			MemeFighter::Tick();
+		}
+	}
+};
+
+class MemeStoner : public MemeFighter
+{
+public:
+	MemeStoner( const std::string& name )
+		:
+		MemeFighter( name,80,4,10 )
+	{}
+	void SpecialMove()
+	{
+		if( IsAlive() )
+		{
+			if( Roll() > 3 )
+			{
+				std::cout << GetName() << " smokes the dank sticky icky, becoming " << "Super " << GetName() << std::endl;
+				name = "Super " + name;
+				speed += 3;
+				power = (power * 69) / 42;
+				hp += 10;
+			}
+			else
+			{
+				std::cout << GetName() << " spaces out." << std::endl;
+			}
+		}
+	}
+};
+
 void Engage( MemeFighter& f1,MemeFighter& f2 )
 {
 	// pointers for sorting purposes
